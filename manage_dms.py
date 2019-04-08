42a4c46515277e2baafdf2fd6527bba065b53edf
Will Grant <will.granty@yahoo.co.uk>
2019-04-08 13:14:43 +0100

    first

---
+++ manage_dms.py
@@ -0,0 +1,31 @@
+'''Currently programme that has ability to check difference between two datamaps'''
+
+from openpyxl import load_workbook, Workbook
+from bcompiler.utils import project_data_from_master
+
+def place_in_excel(list):
+    wb = Workbook()
+    ws = wb.active
+
+    ws.cell(row=1, column=1).value = 'MISSING'
+    for i, key in enumerate(list):
+        ws.cell(row=i+2, column=1).value = key
+
+    return wb
+
+
+gmpp_datamap = project_data_from_master("C:\\Users\\Standalone\\Will\\masters folder\\dms\\gmpp_dm_excel_merging_to_"
+                                        "this_one.xlsx")
+
+internal_datamap = project_data_from_master("C:\\Users\\Standalone\\Will\\masters folder\\dms\\datamap_current_internal"
+                                            ".xlsx")
+
+gmpp_keys = gmpp_datamap['sheet'].keys()
+
+internal_keys = internal_datamap['template_sheet'].keys()
+
+missing_keys = [x for x in internal_keys if x not in gmpp_keys]
+
+output = place_in_excel(missing_keys)
+
+output.save("C:\\Users\\Standalone\\Will\\test.xlsx")
